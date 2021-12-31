67888babe0d4e7ed77bfc4d339eeea9d0f2107b5
Harald-LB <Harald-lb@free-creations.de>
2021-12-31 19:38:21 +0100

    2.the end of Part 2

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,20 +1,34 @@
 import * as React from 'react';
-
 import { Link } from 'gatsby';
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkText,
+  navLinkItem,
+} from './layout.module.css';
 
 // eslint-disable-next-line react/prop-types
 function Layout({ pageTitle, children }) {
   return (
-    <div>
+    <div className={container}>
       <title>{pageTitle}</title>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
         </ul>
       </nav>
       <main>
-        <h1>{pageTitle}</h1>
+        <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
 
