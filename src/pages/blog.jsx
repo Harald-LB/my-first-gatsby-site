4b35153045e944bc2515f28d55492030fbe43951
Harald-LB <Harald-lb@free-creations.de>
2022-01-05 17:46:30 +0100

    within chapter 4

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,12 +1,32 @@
 import * as React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 
-function BlogPage() {
+// eslint-disable-next-line react/prop-types
+function BlogPage({ data }) {
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in here.</p>
+      <ul>
+        {
+          data.allFile.nodes.map((node) => (
+            <li key={node.name}>
+              {node.name}
+            </li>
+          ))
+        }
+      </ul>
     </Layout>
   );
 }
+
+export const query = graphql`
+    query blogQuery {
+        allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+            nodes {
+                name
+            }
+        }
+    }
+`;
 
 export default BlogPage;
